54bcb496e9be040f190f2e489884047436c54d92
Shogo <vanitas710@yahoo.co.jp>
2021-01-21 05:31:57 +0900

    必須となるメソッド定義が不足していてエラーが出るので、 MTKViewDelegateの定義に飛んでメソッドをコピーしてきた。

--- metalInitialize/ViewController.swift
+++ metalInitialize/ViewController.swift
@@ -6,9 +6,23 @@
 //
 
 import UIKit
+import MetalKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MTKViewDelegate{
 
+    @IBOutlet private weak var mtkView: MTKView!
+    
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize){}
+
+    
+    /**
+     @method drawInMTKView:
+     @abstract Called on the delegate when it is asked to render into the view
+     @discussion Called on the delegate when it is asked to render into the view
+     */
+    func draw(in view: MTKView){}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
